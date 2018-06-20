f3f601cc6f9d2c69a60cf72eeb82dd3dd65fe1e2
Augusto Miguel Zeponi <augustozeponi@hotmail.com>
2018-06-19 21:22:19 -0300

    #337 - Cortando e redimensionando imagens

--- app/Http/Controllers/testeImagemController.php
+++ app/Http/Controllers/testeImagemController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Intervention\Image\ImageManagerStatic as Image;
 
 class testeImagemController extends Controller
 {
@@ -24,8 +25,11 @@ class testeImagemController extends Controller
             $imagem = $request->imagem;
             $imagem_nome = time().$imagem->getClientOriginalName();
             $imagem->move("imagem/",$imagem_nome);
-
-            dd($imagem_nome);
+            //$image = Image::make('imagem/'.$imagem_nome)->resize(300, 200)->save("imagem/300_200_".$imagem_nome);
+            $image = Image::make('imagem/'.$imagem_nome)->fit(1200, 600)->brightness(-20)->save("imagem/slide3_".$imagem_nome);
+            $image = Image::make('imagem/'.$imagem_nome)->fit(400, 400)->save("imagem/pequena_".$imagem_nome);
+            $image = Image::make('imagem/'.$imagem_nome)->fit(600, 400)->save("imagem/galeria".$imagem_nome);
+        
         }
         return "Olá sou o #zeponisolucoes";
     }
